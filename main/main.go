1830207c7ee0c336e6fe330eaccf33850b88e4e2
Ilya <ilyail3@gmail.com>
2018-04-04 13:37:29 -0400

    get the config file from the program arguments

--- main/main.go
+++ main/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/ilyail3/stock"
+	"os"
 )
 
 func main(){
-	config, err := stock.ReadConfig("config.json")
-	fmt.Printf("api key:%s\n", config.GetApiKey())
-
+	config, err := stock.ReadConfig(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Printf("api key:%s\n", config.GetApiKey())
 	reader := stock.AlphaAdvantageReader(config.GetApiKey())
 
 	r, err := reader.GetPrice("TSLA")
